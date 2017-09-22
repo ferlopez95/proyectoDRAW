ac5fffdecc1a3ba0e9c1806e9a22e80121355a6a
Unknown <fer14xx@hotmail.com>
2017-09-21 21:22:03 -0500

    new/Parser
    Se agrego el analizador sintactico en el archivo c_lexer.py y se agregaron los ejemplos

--- c_lexer.py
+++ c_lexer.py
@@ -11,7 +11,6 @@ reserved = {
     'int' : 'INT',
     'float' : 'FLOAT',
     'boolean' : 'BOOLEAN',
-    'window' : 'WINDOW',
     'draw' : 'DRAW',
     'array' : 'ARRAY',
     'main' : 'MAIN',
@@ -24,7 +23,6 @@ reserved = {
     'return' : 'RETURN',
     'true' : 'TRUE',
     'false' : 'FALSE',
-    'setBackground' : 'SETBACKGROUND',
     'setPosition' : 'SETPOSITION',
     'setColor' : 'SETCOLOR',
     'speed' : 'SPEED',
@@ -38,7 +36,6 @@ reserved = {
     'square' : 'SQUARE',
     'circle' : 'CIRCLE',
     'length' : 'LENGTH',
-    'Window' : 'NEWWINDOW',
     'Draw' : 'NEWDRAW',
     
 }
@@ -148,32 +145,263 @@ def test(data, lexer):
 
 lexer = lex.lex()
 
-def Analizador_lexico():
-    a = input("direccion: ")
-    if ( os.path.exists (a)):
-        f = open(a)
-        data = f.read()
-        f.close()
-        #Build lexer and try on
-        lexer.input(data)
-        test(data, lexer)
-    else:
-        print ("El archivo no existe")
+#def Analizador_lexico():
+#    a = input("direccion: ")
+#    if ( os.path.exists (a)):
+#        f = open(a)
+#        data = f.read()
+#        f.close()
+#        #Build lexer and try on
+#        lexer.input(data)
+#        test(data, lexer)
+#    else:
+#        print ("El archivo no existe")
 
+VERBOSE = 1
 
-# Test
-if __name__ == '__main__':
+def p_programa(p):
+    '''programa : globales funcion main
+    | main
+    | funcion main'''
+    pass
 
-    # Test  ESTO ES SOLO PARA PROBAR EL FUNCINAMIENTO DE ANIZADOR LEXICO.
-    #Cargamos el archivo "c.cpp" que esta en la carpeta ejemplos y lo guardamos
-    #la variable data para despues enviarla al analizador lexico para que la
-    #descomponga en tokes
+def p_globales(p):
+    '''globales : GLOBAL vars'''
+    pass
 
-    f = open('fuente/c.cpp')
+def p_bloque(p):
+    '''bloque : estatuto bloque
+    | empty'''
+    pass
+
+def p_data_type(p):
+    '''data_type : INT
+    | FLOAT
+    | BOOLEAN'''
+    pass
+
+def p_main(p):
+    '''main : DEF VOID MAIN LPAREN RPAREN bloque END'''
+    pass
+
+def p_estatuto(p):
+    '''estatuto : asignacion
+    | condicion
+    | ciclo
+    | accion
+    | vars
+    | llamada SEMICOLON'''
+    pass
+
+def p_asignacion(p):
+    '''asignacion : ID asignacion_2'''
+    pass
+
+def p_asignacion_2(p):
+    '''asignacion_2 : EQUAL expresion
+    | LBRACKET CTE_I asignacion_3'''
+    pass
+    
+def p_asignacion_3(p):
+    '''asignacion_3 : RBRACKET
+    | COMMA CTE_I RBRACKET'''
+    pass
+
+def p_vars(p):
+    '''vars : DRAW ID EQUAL NEWDRAW LPAREN RPAREN SEMICOLON
+    | data_type ID vars2
+    | ARRAY ID vars3'''
+    pass
+
+def p_vars_2(p):
+    '''vars2 : EQUAL expresion SEMICOLON
+    | SEMICOLON'''
+    pass
+
+def p_vars_3(p):
+    '''vars3 : EQUAL def_array SEMICOLON
+    | SEMICOLON'''
+    pass
+
+def p_llamada(p):
+    '''llamada : ID LPAREN llamada_2'''
+    pass
+
+def p_llamada_2(p):
+    '''llamada_2 : llamada_exp RPAREN
+    | RPAREN'''
+    pass
+
+def p_llamada_exp(p):
+    '''llamada_exp : expresion llamada_exp2'''
+    pass
+
+def p_llamada_exp_2(p):
+    '''llamada_exp2 : COMMA llamada_exp
+    | empty'''
+    pass
+
+def p_def_array(p):
+    '''def_array : LBRACKET def_array_2'''
+    pass
+
+def p_def_array_2(p):
+    '''def_array_2 : def_array_cte RBRACKET
+    | RBRACKET'''
+    pass
+
+def p_def_array_cte(p):
+    '''def_array_cte : var_cte def_array_cte_2'''
+    pass
+
+def p_def_array_cte_2(p):
+    '''def_array_cte_2 : COMMA def_array_cte
+    | empty'''
+    pass
+
+def p_expresion(p):
+    '''expresion : exp expresion_2'''
+    pass
+
+def p_expresion_2(p):
+    '''expresion_2 : relop exp
+    | empty'''
+    pass
+
+def p_relop(p):
+    '''relop : GREATER
+    | GREATEREQUAL
+    | LESS
+    | LESSEQUAL
+    | DEQUAL
+    | DISTINT'''
+    pass
+
+def p_exp(p):
+    '''exp : termino exp_2'''
+    pass
+
+def p_exp_2(p):
+    '''exp_2 : addop exp
+    | empty'''
+    pass
+
+def p_termino(p):
+    '''termino : factor termino_2'''
+    pass
+
+def p_termino_2(p):
+    '''termino_2 : timesop termino
+    | empty'''
+    pass
+
+def p_var_cte(p):
+    '''var_cte : ID
+    | CTE_I
+    | CTE_F
+    | TRUE
+    | FALSE
+    | llamada'''
+    pass
+
+def p_factor(p):
+    '''factor : LPAREN expresion RPAREN
+    | addop var_cte
+    | var_cte
+    | ID LBRACKET exp factor_2'''
+    pass
+
+def p_factor_2(p):
+    '''factor_2 : RBRACKET
+    | COMMA exp RBRACKET'''
+    pass
+
+def p_condicion(p):
+    '''condicion : IF LPAREN expresion RPAREN bloque condicion_2'''
+    pass
+
+def p_condicion_2(p):
+    '''condicion_2 : END
+    | ELSE  bloque END'''
+    pass
+
+def p_ciclo(p):
+    '''ciclo : for
+    | while'''
+    pass
+
+def p_accion(p):
+    '''accion : ID POINT llamada SEMICOLON'''
+    pass
+
+def p_for(p):
+    '''for : FOR LPAREN CTE_I COMMA CTE_I RPAREN bloque END'''
+    pass
+
+def p_while(p):
+    '''while : WHILE LPAREN expresion RPAREN bloque END'''
+    pass
+
+def p_funcion(p):
+    '''funcion :  DEF data_type ID var_local bloque funcion_2'''
+    pass
+
+def p_funcion_2(p):
+    '''funcion_2 : RETURN expresion END
+    | END'''
+    pass
+
+def p_var_local(p):
+    '''var_local : ID LPAREN var_local_2 RPAREN'''
+    pass
+
+def p_var_local_2(p):
+    '''var_local_2 : data_type ID var_local_3'''
+    pass
+
+def p_var_local_3(p):
+    '''var_local_3 : COMMA var_local_2
+    | empty'''
+    pass
+
+def p_addop(p):
+    '''addop : PLUS
+    | MINUS'''
+    pass
+
+def p_timesop(p):
+    '''timesop : TIMES
+    | DIVIDE
+    | PERCENT'''
+    pass
+
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_error(p):
+    print("Error")
+    #print str(dir(p))
+    #print str(dir(c_lexer))
+    #if VERBOSE:
+    #    if p is not None:
+    #        print "Error en Sintaxis linea: " + str(p.lexer.lineno)+ "  Error de Contexto " + str(p.value)
+    #    else:
+    #        print "Error en Lexico linea: " + str(c_lexer.lexer.lineno)
+    #else:
+    #    raise Exception('Syntax', 'error')
+
+import ply.yacc as yacc
+parser = yacc.yacc()
+
+a = input("direccion: ")
+if ( os.path.exists (a)):
+    f = open(a)
     data = f.read()
     f.close()
-    #Build lexer and try on
-    lexer.input(data)
-    test(data, lexer)
+    parser.parse(data)
+else:
+    print("El archivo no existe")
+
 
 
