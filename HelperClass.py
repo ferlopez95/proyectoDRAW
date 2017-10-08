823fb3f565867393fd0c78928c8e14337685f4b2
israel <israel@gmail.com>
2017-10-07 22:54:52 -0500

    Simplify code with functions

--- HelperClass.py
+++ HelperClass.py
@@ -5,23 +5,23 @@ class HelperClass(object):
         self.dir_func = {}
 
     def exists_in_scope(self, id):
-        if (len(self.dir_func[self.actual_scope]['inner_scope']) >= 1):
-            if (id in self.dir_func[self.actual_scope]['vars']) or (id in self.dir_func['global']['vars']) :
+        if (len(self.inner_scopes()) >= 1):
+            if (id in self.vars_table()) or (id in self.global_vars()) :
                 return 1
-            for key in self.dir_func[self.actual_scope]['inner_scope']:
+            for key in self.inner_scopes():
                 if id in key:
                     return 1
         else:
-            if (id in self.dir_func[self.actual_scope]['vars']) or (id in self.dir_func['global']['vars']) :
+            if (id in self.vars_table()) or (id in self.global_vars()) :
                 return 1
 
     def add_var(self, type, id):
-        if (len(self.dir_func[self.actual_scope]['inner_scope']) >= 1):
-            inner = self.dir_func[self.actual_scope]['inner_scope'].pop()
+        if (len(self.inner_scopes()) >= 1):
+            inner = self.inner_scopes().pop()
             inner[id] = {'type' : type}
-            self.dir_func[self.actual_scope]['inner_scope'].append(inner)
+            self.inner_scopes().append(inner)
         else:
-            self.dir_func[self.actual_scope]['vars'][id] = {'type' : type}
+            self.vars_table()[id] = {'type' : type}
 
     def add_func(self, type, id):
         self.dir_func[id] = {'type' : type, 'vars' : {}, 'inner_scope' : []}
@@ -30,7 +30,16 @@ class HelperClass(object):
         return id in self.dir_func
 
     def add_inner_scope(self):
-        self.dir_func[self.actual_scope]['inner_scope'].append({})
+        self.inner_scopes().append({})
 
     def pop_inner_scope(self):
-        self.dir_func[self.actual_scope]['inner_scope'].pop()
+        self.inner_scopes().pop()
+
+    def inner_scopes(self):
+        return self.dir_func[self.actual_scope]['inner_scope']
+
+    def vars_table(self):
+        return self.dir_func[self.actual_scope]['vars']
+
+    def global_vars(self):
+        return self.dir_func['global']['vars']
